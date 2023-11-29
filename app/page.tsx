e8d0ca79455e3b1a0cd11822db4b4d293bfbf07d
William Santos <williambatista958@gmail.com>
2023-11-29 01:15:27 -0300

    feat: criando página de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,42 @@
-import Link from "next/link";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.scss";
 import Input from "./components/Input/Input";
-import styles from "./page.module.css"
+import Link from "next/link";
+import Button from "./components/Button/Button";
+const registerImage = "register.png";
 
 export default function Home() {
-  return (
-    <div>
-      <h1> Olá,</h1>
-      <h2> Por favor, registra-se para continuar.</h2>
-      <form action="" className={styles.form}>
-        <h1> Cadastro</h1>
-        <Input type="text" isRequired placeholder="Nome" name="name" icon="user"/>
-        <Input type="text" isRequired placeholder="Usuário" name="name" icon="username"/>
-        <Input type="date" isRequired placeholder="Nascimento" name="nascimento" icon="date"/>
-        <Input type="email" isRequired placeholder="Email" name="email" icon="email"/>
-        <Input type="text" isRequired placeholder="Senha" name="senha" icon="padlock"/>
-        <Input type="text" isRequired placeholder="Confirmar Senha" name="confirmesenha" icon="security"/>
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState("");
+  const handleLoginSubmit = () => {
+    console.log("Sumeteu");
+  };
 
-        <button>Registra-se</button>
-        <h2>Já possui uma conta? <Link href={"/login"}>Faça Login</Link></h2>
+  return (
+    <div className={styles.maincontent}>
+      <form className={styles.form}>
+        <div className={styles.formcontent}>
+          <div className={styles.infoform}>
+            <h1> Olá,</h1>
+            <h2> Para continuar navegando de forma segura, efetue o login.</h2>
+            <h1 className={styles.titlelogin}>Login</h1>
+          </div>
+          <Input isRequired  type={"text"} placeholder={"Usuário"}
+            icon={"user"} onChange={(e: any) => { setUser(e.target.value);}} value={user} isValid={""}/>
+          <Input isRequired type={"text"} placeholder={"Senha"}
+            icon={"padlock"} onChange={(e: any) => { setPassword(e.target.value);}} value={password}isValid={""}/>
+        </div>
+        <Button type="submit" text="Entrar" />
+        <h2 className={styles.register}>
+          Novo por aqui?{" "}
+          <Link href={"/register"}>
+            <span>Registre-se</span>
+          </Link>
+        </h2>
       </form>
-      
+      <img className={styles.image} src={registerImage} alt="Image computer" />
     </div>
   );
 }
