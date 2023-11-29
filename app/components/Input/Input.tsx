d2ed8bcb2ca6aac15ab9c78232a512020ed6735a
William Santos <williambatista958@gmail.com>
2023-11-29 01:12:58 -0300

    feat: criando input

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,23 +1,35 @@
+"use client"
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import styles from "./Input.module.scss";
+import styles from "./Input.module.scss"
 
 interface inputProps{
     type: string;
-    name: string;
     placeholder: string;
     isRequired?: boolean;
     icon: string;
+    onChange: any;
+    value: any;
+    isValid: boolean | string;
 }
 
 export default function Input(props:inputProps) {
+  var validate = styles.default
+  if(props.isValid !== true && props.isValid !== "default" ){
+    validate = styles.invalid
+  }
+  if(props.isValid === true){
+    validate = styles.valid
+  }
+
   return (
-    <div className={styles.inputcontainer}>
+    <div className={[styles.inputcontainer, validate].join(' ')}>
       <input
         type={props.type}
-        name={props.name}
         placeholder={props.placeholder}
         required={props.isRequired}
+        onChange={props.onChange}
+        value={props.value}
       />
       <img src={`/icons/${props.icon}.svg` }alt={`${props.icon} Icon`} />
       
