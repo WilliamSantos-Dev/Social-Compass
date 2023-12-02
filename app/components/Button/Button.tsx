3c18b2b255bd3997d9a9916fe9e1eac8262c6a32
William Santos <williambatista958@gmail.com>
2023-12-02 00:23:39 -0300

    refactor: deixando componente button ajustavel

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -3,11 +3,16 @@ import styles from "./Button.module.scss"
 interface Props{
     text: string;
     type: "button" | "submit";
+    action?: any;
+    width?: number;
 }
-export default function Button(props: Props){
+export default function Button(props: Props){   
+    const buttonWidth = {
+        width: `${props.width ?? 406}px`,
+    };
     return(
         <>
-        <button className={styles.button} type={props.type}>
+        <button className={styles.button} style={buttonWidth} type={props.type}>
           {props.text}
         </button>
         </>
