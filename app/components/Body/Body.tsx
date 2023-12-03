6962578d7e08789dd5d9343dc8cebe2222dda986
William Santos <williambatista958@gmail.com>
2023-12-02 22:01:38 -0300

    refactor: fixando header a ajustando bugs do body

--- app/components/Body/Body.tsx
+++ app/components/Body/Body.tsx
@@ -18,24 +18,43 @@ export default function Body({ children, user }: Props) {
     setMenuOpen(!menuOpen);
   };
 
+  var stylerHeader = styles.fixedMenuOpenHeader;
+  var stylechildren = styles.childrenMenuOpen;
+
+  if (!menuOpen) {
+    stylerHeader = styles.fixedMenuClose;
+    stylechildren = styles.children;
+  }
+
   return (
     <div className={styles.maincontent}>
-      <AnimatePresence>
-        {menuOpen && (
-          <motion.div
-            key="menu"
-            initial={{ opacity: 0, x: "-100%" }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: "-100%" }}
-            transition={{ duration: 0.2 }}
-          >
-            <Menu />
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <div>
-        <Header userimage={user.image} username={user.name} menuOpen={menuOpen} updateMenu={updateMenu} />
-        {children}
+      <div className={styles.menu}>
+        <AnimatePresence>
+          {menuOpen && (
+            <motion.div
+              key="menu"
+              initial={{ opacity: 0, x: "-100%" }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: "-100%" }}
+              transition={{ duration: 0.15 }}
+            >
+              <div className={styles.menucontent}>
+                <Menu />
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div className={styles.childrencontainer}>
+        <div className={stylerHeader}>
+          <Header
+            userimage={user.image}
+            username={user.name}
+            menuOpen={menuOpen}
+            updateMenu={updateMenu}
+          />
+        </div>
+        <div className={stylechildren}>{children}</div>
       </div>
     </div>
   );
